3c92df7ecb08874d1a1c1cb8d2c7cd5d28dc0b45
sirily11 <sirily1997@gmail.com>
2023-01-27 10:57:48 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SwiftUIJsonSchemaForm
+
+A description of this package.
---
+++ Sources/SwiftUIJsonSchemaForm/SwiftUIJsonSchemaForm.swift
@@ -0,0 +1,6 @@
+public struct SwiftUIJsonSchemaForm {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
